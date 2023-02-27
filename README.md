6cce95c9c5b3ff43c33a67cbfcadceab44404fa4
chenrujing.ovo <chenrujing.ovo@bytedance.com>
2023-02-27 19:38:01 +0800

    feat: webpack工程化

---
+++ README.md
@@ -0,0 +1,3 @@
+# React18_TS4_Webpack5
+
+https://juejin.cn/post/7203169721839042615
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import '@/App.css'
+import React from 'react'
+
+function App() {
+  return <h2>webpack5-react-ts</h2>
+}
+
+export default App
